1bbd783e0296f9f5e58c6777f58b20d55fc7e33a
Sevval Mertoglu <sevvalmertoglu131@gmail.com>
2024-07-16 18:15:07 +0300

    first commit

---
+++ QuizApp_SwiftUI/QuizApp_SwiftUI/QuizApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizApp_SwiftUIApp.swift
+//  QuizApp_SwiftUI
+//
+//  Created by Şevval Mertoğlu on 16.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct QuizApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
