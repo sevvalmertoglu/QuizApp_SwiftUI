0cfbe81d2e79281d9289536d2d7e70cafdf59307
Sevval Mertoglu <sevvalmertoglu131@gmail.com>
2024-08-14 18:00:05 +0300

    I made sure the information on the sheet was kept correct.

--- QuizApp_SwiftUI/QuizApp_SwiftUI/LeaderBoard/LeaderRankingView.swift
+++ QuizApp_SwiftUI/QuizApp_SwiftUI/LeaderBoard/LeaderRankingView.swift
@@ -11,43 +11,46 @@ struct LeaderRankingView: View {
     @StateObject private var viewModel = LeaderRankingViewModel()
 
     var body: some View {
-        VStack(spacing: 10) {
-            ForEach(0..<self.viewModel.otherUsers.count, id: \.self) { index in
-                HStack {
-                    ZStack {
-                        Circle()
-                            .stroke(Color.gray, lineWidth: 2)
-                            .frame(width: 30, height: 30)
+        ScrollView {
+            VStack {
+                ForEach(0..<self.viewModel.otherUsers.count, id: \.self) { index in
+                    HStack {
+                        ZStack {
+                            Circle()
+                                .stroke(Color.gray, lineWidth: 2)
+                                .frame(width: 30, height: 30)
 
-                        Text("\(index + 4)")
-                            .font(.system(size: 16, weight: .medium))
+                            Text("\(index + 4)")
+                                .font(.system(size: 16, weight: .medium))
+                                .foregroundColor(.black)
+                        }
+                        .frame(width: 40)
+
+                        Image(self.viewModel.otherUsers[index].userIcon) // User image
+                            .resizable()
+                            .frame(width: 40, height: 40)
+                            .clipShape(Circle())
+                            .padding(.trailing, 10)
+
+                        VStack(alignment: .leading) {
+                            Text(self.viewModel.otherUsers[index].nickname) // User name
+                                .font(.system(size: 18, weight: .bold))
+                                .foregroundColor(.black)
+
+                            Text("\(self.viewModel.otherUsers[index].totalScore) point") // User points
+                                .font(.system(size: 14))
+                                .foregroundColor(.gray)
+                        }
+
+                        Spacer()
                     }
-                    .frame(width: 40)
-
-                    Image(self.viewModel.otherUsers[index].userIcon) // User image
-                        .resizable()
-                        .frame(width: 40, height: 40)
-                        .clipShape(Circle())
-                        .padding(.trailing, 10)
-
-                    VStack(alignment: .leading) {
-                        Text(self.viewModel.otherUsers[index].nickname) // User name
-                            .font(.system(size: 18, weight: .bold))
-                            .foregroundColor(.black)
-
-                        Text("\(self.viewModel.otherUsers[index].totalScore) point") // User points
-                            .font(.system(size: 14))
-                            .foregroundColor(.gray)
-                    }
-
-                    Spacer()
+                    .padding()
+                    .background(Color.white)
+                    .cornerRadius(15)
+                    .shadow(radius: 3)
                 }
-                .padding()
-                .background(Color.white)
-                .cornerRadius(15)
-                .shadow(radius: 3)
             }
+            .padding()
         }
-        .padding()
     }
 }
