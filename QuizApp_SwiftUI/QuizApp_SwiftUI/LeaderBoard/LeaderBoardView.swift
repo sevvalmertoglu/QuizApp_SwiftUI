986d9e8522de1e78f4b29730b0fbe6cdb8d8bda2
Sevval Mertoglu <sevvalmertoglu131@gmail.com>
2024-08-22 13:47:31 +0300

    added custom sheet

--- QuizApp_SwiftUI/QuizApp_SwiftUI/LeaderBoard/LeaderBoardView.swift
+++ QuizApp_SwiftUI/QuizApp_SwiftUI/LeaderBoard/LeaderBoardView.swift
@@ -9,111 +9,128 @@ import SwiftUI
 
 struct LeaderBoardView: View {
     @StateObject private var viewModel = LeaderBoardViewModel()
-    @State private var showCategorySelector: Bool = true
+    @State private var shouldShownSheet: Bool = true
     @State private var isAnimating: Bool = false
     @Namespace private var namespace
-    @State var isClicked: Bool = false
+
+    @State var startingOffsetY: CGFloat = UIScreen.main.bounds.height * 0.55
+    @State var currentDragOffsetY: CGFloat = 0
+    @State var endingOffsetY: CGFloat = 0
 
     var body: some View {
-        VStack {
-            if self.viewModel.isLoading {
-                ProgressView("Loading Leaderboard...")
-                    .progressViewStyle(CircularProgressViewStyle(tint: .white))
-                    .scaleEffect(1.5)
-                    .padding(.top, 50)
-            } else {
-                ZStack(alignment: .topTrailing) {
-                    if !self.isClicked {
-                        Button(action: {
-                            withAnimation(.easeInOut(duration: 0.5)) {
-                                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                                    self.showCategorySelector = true
+        ZStack {
+            VStack {
+                if self.viewModel.isLoading {
+                    ProgressView("Loading Leaderboard...")
+                        .progressViewStyle(CircularProgressViewStyle(tint: .white))
+                        .scaleEffect(1.5)
+                        .padding(.top, 50)
+                } else {
+                    ZStack(alignment: .topTrailing) {
+                        if !self.shouldShownSheet {
+                            Button(action: {
+                                withAnimation(.easeInOut(duration: 0.5)) {
+                                    self.shouldShownSheet.toggle()
                                 }
-                                self.isClicked.toggle()
+                            }) {
+                                Text("Other Scores")
+                                    .foregroundColor(.white)
+                                    .padding(6)
+                                    .background(Color.green)
+                                    .cornerRadius(15)
+                                    .matchedGeometryEffect(id: "heroButton", in: self.namespace)
                             }
-                        }) {
-                            Text("Other Scores")
-                                .foregroundColor(.white)
-                                .padding(6)
-                                .background(Color.green)
-                                .cornerRadius(15)
-                                .matchedGeometryEffect(id: "heroButton", in: self.namespace)
+                            .padding()
                         }
-                        .offset(y: self.isClicked ? UIScreen.main.bounds.height : 0)
-                        .padding()
+
+                        Spacer()
+
+                        HStack(spacing: 0) {
+                            // Left Stack (Rank 2)
+                            if self.viewModel.topThreeUsers.count > 1 {
+                                LeaderBoardStack(
+                                    rankImage: QuizAppImages.instance.Rank2.asImage,
+                                    medalImage: QuizAppImages.instance.Medal2.asImage,
+                                    userImage: self.viewModel.topThreeUsers[1].userIcon,
+                                    username: self.viewModel.topThreeUsers[1].nickname,
+                                    score: "\(self.viewModel.topThreeUsers[1].totalScore) point",
+                                    width: 90,
+                                    height: 240,
+                                    rankOffset: 30
+                                )
+                            }
+
+                            // Center Stack (Rank 1)
+                            if !self.viewModel.topThreeUsers.isEmpty {
+                                LeaderBoardStack(
+                                    rankImage: QuizAppImages.instance.Rank1.asImage,
+                                    medalImage: QuizAppImages.instance.Medal1.asImage,
+                                    userImage: self.viewModel.topThreeUsers[0].userIcon,
+                                    username: self.viewModel.topThreeUsers[0].nickname,
+                                    score: "\(self.viewModel.topThreeUsers[0].totalScore) point",
+                                    width: 100,
+                                    height: 280,
+                                    rankOffset: 0
+                                )
+                            }
+
+                            // Right Stack (Rank 3)
+                            if self.viewModel.topThreeUsers.count > 2 {
+                                LeaderBoardStack(
+                                    rankImage: QuizAppImages.instance.Rank3.asImage,
+                                    medalImage: QuizAppImages.instance.Medal3.asImage,
+                                    userImage: self.viewModel.topThreeUsers[2].userIcon,
+                                    username: self.viewModel.topThreeUsers[2].nickname,
+                                    score: "\(self.viewModel.topThreeUsers[2].totalScore) point",
+                                    width: 90,
+                                    height: 240,
+                                    rankOffset: 40
+                                )
+                            }
+                        }
+                        .padding([.horizontal, .top], 20)
+                        .padding(.bottom, 120)
+                        .frame(maxHeight: .infinity)
                     }
-
-                    Spacer()
-
-                    HStack(spacing: 0) {
-                        // Left Stack (Rank 2)
-                        if self.viewModel.topThreeUsers.count > 1 {
-                            LeaderBoardStack(
-                                rankImage: QuizAppImages.instance.Rank2.asImage,
-                                medalImage: QuizAppImages.instance.Medal2.asImage,
-                                userImage: self.viewModel.topThreeUsers[1].userIcon,
-                                username: self.viewModel.topThreeUsers[1].nickname,
-                                score: "\(self.viewModel.topThreeUsers[1].totalScore) point",
-                                width: 90,
-                                height: 240,
-                                rankOffset: 30
-                            )
-                        }
-
-                        // Center Stack (Rank 1)
-                        if !self.viewModel.topThreeUsers.isEmpty {
-                            LeaderBoardStack(
-                                rankImage: QuizAppImages.instance.Rank1.asImage,
-                                medalImage: QuizAppImages.instance.Medal1.asImage,
-                                userImage: self.viewModel.topThreeUsers[0].userIcon,
-                                username: self.viewModel.topThreeUsers[0].nickname,
-                                score: "\(self.viewModel.topThreeUsers[0].totalScore) point",
-                                width: 100,
-                                height: 280,
-                                rankOffset: 0
-                            )
-                        }
-
-                        // Right Stack (Rank 3)
-                        if self.viewModel.topThreeUsers.count > 2 {
-                            LeaderBoardStack(
-                                rankImage: QuizAppImages.instance.Rank3.asImage,
-                                medalImage: QuizAppImages.instance.Medal3.asImage,
-                                userImage: self.viewModel.topThreeUsers[2].userIcon,
-                                username: self.viewModel.topThreeUsers[2].nickname,
-                                score: "\(self.viewModel.topThreeUsers[2].totalScore) point",
-                                width: 90,
-                                height: 240,
-                                rankOffset: 40
-                            )
-                        }
-                    }
-                    .padding([.horizontal, .top], 20)
-                    .padding(.bottom, 120)
-                    .frame(maxHeight: .infinity)
-                }
-                .background(
-                    QuizAppImages.instance.backgroundLeaderBoard.asImage
-                        .resizable()
-                        .scaledToFill()
-                        .ignoresSafeArea(.all)
-                )
-                .sheet(isPresented: self.$showCategorySelector, onDismiss: {}) {
-                    LeaderRankingView(namespace: self.namespace, isClicked: self.$isClicked)
-                        .presentationDetents([.medium, .fraction(0.8), .height(300)])
-                        .presentationBackground(.thinMaterial)
-                        .presentationCornerRadius(50)
-                        .presentationDragIndicator(.hidden)
+                    .background(
+                        QuizAppImages.instance.backgroundLeaderBoard.asImage
+                            .resizable()
+                            .scaledToFill()
+                            .ignoresSafeArea(.all)
+                    )
                 }
             }
-        }
-        .onTapGesture {
-            withAnimation(.easeInOut) {
-                self.isClicked.toggle()
+
+            if self.shouldShownSheet {
+                // Custom Sheet
+                LeaderRankingView(namespace: self.namespace, shouldShownSheet: self.$shouldShownSheet)
+                    .offset(y: self.startingOffsetY)
+                    .offset(y: self.currentDragOffsetY)
+                    .offset(y: self.endingOffsetY)
+                    .gesture(
+                        DragGesture()
+                            .onChanged { value in
+                                withAnimation(.spring()) {
+                                    self.currentDragOffsetY = value.translation.height
+                                }
+                            }
+                            .onEnded { _ in
+                                withAnimation(.spring()) {
+                                    if self.currentDragOffsetY < -150 {
+                                        self.endingOffsetY = -self.startingOffsetY
+                                    } else if self.endingOffsetY != 0, self.currentDragOffsetY > 150 {
+                                        self.endingOffsetY = 0
+                                    }
+                                    self.currentDragOffsetY = 0
+                                }
+                            }
+                    )
+                    .transition(.move(edge: .bottom))
+                    .animation(.easeInOut(duration: 0.5), value: self.shouldShownSheet)
             }
         }
         .onAppear {
-            self.showCategorySelector = true
+            self.shouldShownSheet = true
         }
     }
 }
--- QuizApp_SwiftUI/QuizApp_SwiftUI/LeaderBoard/LeaderRankingView.swift
+++ QuizApp_SwiftUI/QuizApp_SwiftUI/LeaderBoard/LeaderRankingView.swift
@@ -11,70 +11,72 @@ struct LeaderRankingView: View {
     @StateObject private var viewModel = LeaderRankingViewModel()
     var namespace: Namespace.ID
     @Environment(\.dismiss) private var dismiss
-    @Binding var isClicked: Bool
+    @Binding var shouldShownSheet: Bool
 
     var body: some View {
-        ScrollView {
+        VStack {
             HStack {
                 Spacer()
-                if self.isClicked {
-                    Button(action: {
-                        withAnimation(.easeInOut(duration: 0.5)) {
-                            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                                self.dismiss()
-                            }
-                            self.isClicked.toggle()
-                        }
-                    }, label: {
-                        Image(systemName: "xmark.circle.fill")
-                            .resizable()
-                            .foregroundStyle(.white)
-                            .frame(width: 25, height: 25)
-                            .padding([.top, .trailing], 25)
-                            .matchedGeometryEffect(id: "heroButton", in: self.namespace) // Hero effect
-                    })
-                }
-            }
 
-            VStack {
-                ForEach(0..<self.viewModel.otherUsers.count, id: \.self) { index in
-                    HStack {
-                        ZStack {
-                            Circle()
-                                .stroke(Color.gray, lineWidth: 2)
-                                .frame(width: 30, height: 30)
-
-                            Text("\(index + 4)")
-                                .font(.system(size: 16, weight: .medium))
-                                .foregroundColor(.black)
-                        }
-                        .frame(width: 40)
-
-                        Image(self.viewModel.otherUsers[index].userIcon) // User image
-                            .resizable()
-                            .frame(width: 40, height: 40)
-                            .clipShape(Circle())
-                            .padding(.trailing, 10)
-
-                        VStack(alignment: .leading) {
-                            Text(self.viewModel.otherUsers[index].nickname) // User name
-                                .font(.system(size: 18, weight: .bold))
-                                .foregroundColor(.black)
-
-                            Text("\(self.viewModel.otherUsers[index].totalScore) point") // User points
-                                .font(.system(size: 14))
-                                .foregroundColor(.gray)
-                        }
-
-                        Spacer()
+                Button(action: {
+                    withAnimation(.easeInOut(duration: 0.7)) {
+                        self.shouldShownSheet.toggle()
                     }
-                    .padding()
-                    .background(Color.white)
-                    .cornerRadius(15)
-                    .shadow(radius: 3)
-                }
+                }, label: {
+                    Image(systemName: "xmark.circle.fill")
+                        .resizable()
+                        .foregroundStyle(.white)
+                        .frame(width: 25, height: 25)
+                        .padding([.top, .trailing], 25)
+                        .matchedGeometryEffect(id: "heroButton", in: self.namespace) // Hero effect
+                })
             }
-            .padding()
+
+            ScrollView {
+                VStack {
+                    ForEach(0..<self.viewModel.otherUsers.count, id: \.self) { index in
+                        HStack {
+                            ZStack {
+                                Circle()
+                                    .stroke(Color.gray, lineWidth: 2)
+                                    .frame(width: 30, height: 30)
+
+                                Text("\(index + 4)")
+                                    .font(.system(size: 16, weight: .medium))
+                                    .foregroundColor(.black)
+                            }
+                            .frame(width: 40)
+
+                            Image(self.viewModel.otherUsers[index].userIcon) // User image
+                                .resizable()
+                                .frame(width: 40, height: 40)
+                                .clipShape(Circle())
+                                .padding(.trailing, 10)
+
+                            VStack(alignment: .leading) {
+                                Text(self.viewModel.otherUsers[index].nickname) // User name
+                                    .font(.system(size: 18, weight: .bold))
+                                    .foregroundColor(.black)
+
+                                Text("\(self.viewModel.otherUsers[index].totalScore) point") // User points
+                                    .font(.system(size: 14))
+                                    .foregroundColor(.gray)
+                            }
+
+                            Spacer()
+                        }
+                        .padding()
+                        .background(Color.white)
+                        .cornerRadius(15)
+                        .shadow(radius: 3)
+                    }
+                }
+                .padding()
+            }
         }
+
+        .frame(maxWidth: .infinity)
+        .background(.thinMaterial)
+        .cornerRadius(50)
     }
 }
