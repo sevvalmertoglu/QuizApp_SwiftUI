85c4efbae7dd5a780ce7acc33299f144f6a524ec
Sevval Mertoglu <sevvalmertoglu131@gmail.com>
2024-09-12 17:20:46 +0300

    deleted ScrollView

--- QuizApp_SwiftUI/QuizApp_SwiftUI/LeaderBoard/LeaderRankingView.swift
+++ QuizApp_SwiftUI/QuizApp_SwiftUI/LeaderBoard/LeaderRankingView.swift
@@ -17,7 +17,6 @@ struct LeaderRankingView: View {
         VStack {
             HStack {
                 Spacer()
-
                 Button(action: {
                     withAnimation(.easeInOut(duration: 0.7)) {
                         self.shouldShownSheet.toggle()
@@ -31,50 +30,46 @@ struct LeaderRankingView: View {
                         .matchedGeometryEffect(id: "heroButton", in: self.namespace) // Hero effect
                 })
             }
+            VStack {
+                ForEach(0..<self.viewModel.otherUsers.count, id: \.self) { index in
+                    HStack {
+                        ZStack {
+                            Circle()
+                                .stroke(Color.gray, lineWidth: 2)
+                                .frame(width: 30, height: 30)
 
-            ScrollView {
-                VStack {
-                    ForEach(0..<self.viewModel.otherUsers.count, id: \.self) { index in
-                        HStack {
-                            ZStack {
-                                Circle()
-                                    .stroke(Color.gray, lineWidth: 2)
-                                    .frame(width: 30, height: 30)
-
-                                Text("\(index + 4)")
-                                    .font(.system(size: 16, weight: .medium))
-                                    .foregroundColor(.black)
-                            }
-                            .frame(width: 40)
-
-                            Image(self.viewModel.otherUsers[index].userIcon) // User image
-                                .resizable()
-                                .frame(width: 40, height: 40)
-                                .clipShape(Circle())
-                                .padding(.trailing, 10)
-
-                            VStack(alignment: .leading) {
-                                Text(self.viewModel.otherUsers[index].nickname) // User name
-                                    .font(.system(size: 18, weight: .bold))
-                                    .foregroundColor(.black)
-
-                                Text("\(self.viewModel.otherUsers[index].totalScore) point") // User points
-                                    .font(.system(size: 14))
-                                    .foregroundColor(.gray)
-                            }
-
-                            Spacer()
+                            Text("\(index + 4)")
+                                .font(.system(size: 16, weight: .medium))
+                                .foregroundColor(.black)
                         }
-                        .padding()
-                        .background(Color.white)
-                        .cornerRadius(15)
-                        .shadow(radius: 3)
-                    }
-                }
-                .padding()
-            }
-        }
+                        .frame(width: 40)
 
+                        Image(self.viewModel.otherUsers[index].userIcon) // User image
+                            .resizable()
+                            .frame(width: 40, height: 40)
+                            .clipShape(Circle())
+                            .padding(.trailing, 10)
+
+                        VStack(alignment: .leading) {
+                            Text(self.viewModel.otherUsers[index].nickname) // User name
+                                .font(.system(size: 18, weight: .bold))
+                                .foregroundColor(.black)
+
+                            Text("\(self.viewModel.otherUsers[index].totalScore) point") // User points
+                                .font(.system(size: 14))
+                                .foregroundColor(.gray)
+                        }
+
+                        Spacer()
+                    }
+                    .padding()
+                    .background(Color.white)
+                    .cornerRadius(15)
+                    .shadow(radius: 3)
+                }
+            }
+            .padding()
+        }
         .frame(maxWidth: .infinity)
         .background(.thinMaterial)
         .cornerRadius(50)
